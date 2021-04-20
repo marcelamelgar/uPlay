6938a568f5da3ca24b4171fd50b8c61e62b51aa8
marcelamelgar <61280126+marcelamelgar@users.noreply.github.com>
2021-04-20 10:19:20 -0600

    home, signup, login

---
+++ prueba/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, url_for, redirect
+from jinja2 import Template, FileSystemLoader, Environment
+
+domain = "0.0.0.0:5000/"
+templates = FileSystemLoader('templates')
+environment = Environment(loader = templates)
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("home.html")
+
+@app.route("/contact", methods=["GET","POST"])
+def contact():
+    return render_template("contact.html")
+
+@app.route("/login", methods=["GET","POST"])   
+def login():
+    return render_template("login.html")
+
+@app.route("/signup", methods=["GET","POST"])
+def signup():
+    return render_template("signup.html")
+
+@app.route("/terms", methods=["GET","POST"])
+def terms():
+    return render_template("term.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
